cdf671c556e9bdab57dcd6cb1b95a63421c11bc3
Neil Gebhard <neilgebhard@gmail.com>
2021-10-27 09:58:27 +1000

    Changed minor text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload!!
+          <code>Imagine this being a new feature!</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is a new feature!
+          Learn React
         </a>
       </header>
     </div>
