075cba282475f4e03e1a798af4eeb0afe207b154
Neil Gebhard <neilgebhard@gmail.com>
2021-10-27 09:52:40 +1000

    Changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a new feature!
         </a>
       </header>
     </div>
